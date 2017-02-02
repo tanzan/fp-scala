160991f0579aa4403e1ca9a597836e8da96fb8c4
Serg Zubovich <sergiy.zubovich@sintez.co.za>
2017-02-02 13:59:30 +0200

    Initital commit

---
+++ src/main/scala/MyList.scala
@@ -0,0 +1,54 @@
+/**
+  * Created by serg on 2/2/17.
+  */
+trait MyList[+T] {
+
+  def head:T
+
+  def tail: MyList[T]
+
+  def isEmpty():Boolean
+
+  def ::[B >: T](h:B):MyList[B] = new ::(h, this)
+
+  def reverse:MyList[T] = {
+    def iterate(l:MyList[T], r:MyList[T]):MyList[T] = {
+      if (l.isEmpty()) r
+      else iterate(l.tail, l.head :: r)
+    }
+    iterate(this, MyNil)
+  }
+
+  override def toString: String = {
+    def iterate(l:MyList[_], s: String):String = {
+      if (l.isEmpty()) s + "Nil"
+      else iterate(l.tail, s  + l.head.toString + "::")
+    }
+    iterate(this, "")
+  }
+
+}
+
+object MyNil extends MyList[Nothing] {
+  def head: Nothing =  throw new NoSuchElementException
+
+  def tail: MyList[Nothing] = throw new UnsupportedOperationException
+
+  def isEmpty(): Boolean = true
+}
+
+case class ::[T](h:T, t:MyList[T]) extends MyList[T] {
+
+  def head: T = h
+
+  def tail: MyList[T] = t
+
+  def isEmpty(): Boolean = false
+}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println(1::2::3::MyNil)
+    println((1::2::3::MyNil).reverse)
+  }
+}
