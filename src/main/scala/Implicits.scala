163badc7ba8192a5d35eb43d5777283094298aa2
Serg Zubovich <sergiy.zubovich@sintez.co.za>
2017-02-08 20:02:55 +0200

    additional list methods

---
+++ src/main/scala/Implicits.scala
@@ -0,0 +1,41 @@
+/**
+  * Created by serg on 2/7/17.
+  */
+object Implicits {
+
+  import scala.language.implicitConversions
+
+  implicit def doubleToInt(d:Double):Int = d.toInt
+
+  val i:Int = 3.5
+
+  class A
+
+  class B {
+    def doIt():Unit = ()
+  }
+
+  implicit def a2b(a:A):B = new B
+
+
+  new A().doIt
+
+
+  class PreferredPrompt(val pref:String)
+
+  object Greeter {
+    def greet(name:String)(implicit prompt:PreferredPrompt, company:String):Unit = {
+      println("Hello " + name + " from " + company)
+      println(prompt.pref)
+    }
+  }
+
+  implicit val bash = new PreferredPrompt(">")
+
+  implicit val company = "Sintez"
+
+  def main(args: Array[String]): Unit = {
+    Greeter.greet("Serg")
+  }
+
+}
--- src/main/scala/MyList.scala
+++ src/main/scala/MyList.scala
@@ -1,26 +1,55 @@
+import scala.annotation.tailrec
+import scala.collection.mutable.ListBuffer
+
 /**
   * Created by serg on 2/2/17.
   */
-trait MyList[+T] {
+abstract class MyList[+T] {
 
   def head:T
 
   def tail: MyList[T]
 
-  def isEmpty():Boolean
+  def isEmpty:Boolean
 
   def ::[B >: T](h:B):MyList[B] = new ::(h, this)
 
-  /*def :::[B >: T](list:MyList[B]):MyList[B] = {
-    def iterate(l:MyList[T], r:MyList[B]):MyList[B] = {
+  def :::[B >: T](prefix:MyList[B]):MyList[B] =
+    if (prefix.isEmpty) this
+    else prefix.head::prefix.tail:::this
 
-    }
-    iterate()
-  }*/
+  def size:Int =
+    if(isEmpty) 0 else 1 + tail.size
+
+
+  def drop(n:Int):MyList[T] =
+    if (n <= 0) this
+    else tail.drop(n - 1)
+
+  def dropRight(n:Int):MyList[T] = {
+    def copy(l:MyList[T], r:MyList[T]):MyList[T] =
+      if (r.isEmpty) MyNil
+      else l.head::copy(l.tail, r.tail)
+    copy(this, drop(n))
+  }
+
+  def take(n:Int):MyList[T] = {
+    if(n <= 0) MyNil
+    else head::tail.take(n - 1)
+  }
+
+  def map[B](f:T => B):MyList[B] =
+    if (isEmpty) MyNil
+    else f(head) :: tail.map(f)
+
+  def flatMap[B](f: T => MyList[B]):MyList[B] =
+    if (isEmpty) MyNil
+    else f(head) ::: tail.flatMap(f)
 
   def reverse:MyList[T] = {
+    @tailrec
     def iterate(l:MyList[T], r:MyList[T]):MyList[T] = {
-      if (l.isEmpty()) r
+      if (l.isEmpty) r
       else iterate(l.tail, l.head :: r)
     }
     iterate(this, MyNil)
@@ -28,23 +57,23 @@ trait MyList[+T] {
 
   override def toString: String = {
     def iterate(l:MyList[_], s: String):String = {
-      if (l.isEmpty()) s + "Nil"
-      else iterate(l.tail, s  + l.head.toString + "::")
+      if (l.isEmpty) s + ")"
+      else iterate(l.tail, s + l.head.toString + (if (l.tail.isEmpty) "" else  "," ))
     }
-    iterate(this, "")
+    iterate(this, "MyList(")
   }
 
 }
 
-object MyNil extends MyList[Nothing] {
+case object MyNil extends MyList[Nothing] {
   def head: Nothing =  throw new NoSuchElementException
 
-  def tail: MyList[Nothing] = throw new UnsupportedOperationException
+  def tail: MyList[Nothing] = throw new NoSuchElementException
 
   def isEmpty(): Boolean = true
 }
 
-case class ::[T](h:T, t:MyList[T]) extends MyList[T] {
+final case class ::[T](h:T, t:MyList[T]) extends MyList[T] {
 
   def head: T = h
 
@@ -53,14 +82,24 @@ case class ::[T](h:T, t:MyList[T]) extends MyList[T] {
   def isEmpty(): Boolean = false
 }
 
-class A
-
-class B extends A
+object MyList {
+  def apply[T](elems:T *):MyList[T] =
+    if (elems.isEmpty) MyNil
+    else elems.head :: apply[T](elems.tail:_ *)
+}
 
 object Main {
   def main(args: Array[String]): Unit = {
     println(1::2::3::MyNil)
     println((1::2::3::MyNil).reverse)
+
+    println((1::2::MyNil):::(3::4::MyNil))
+    println(MyList(1,2,3))
+    println(MyList(1,2,3).drop(2))
+    println(MyList(1,2,3).map(_ * 2))
+    println(MyList(1,2,3).flatMap(x => MyList(x * x)))
+    println(MyList(1,2,3,4).dropRight(2))
+    println(MyList(1,2,3,4).take(2))
 
   }
 }
